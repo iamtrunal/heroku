26f5e4769bf89530b37b057eee5699861329e590
Trunal Dudhatra <trunaldudhatra@gmail.com>
2021-12-11 19:24:42 +0530

    ADD:database connected

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://TD:td_Database@cluster0.mfeba.mongodb.net/test")
+mongoose.connect("mongodb+srv://TD:td_Database@cluster0.mfeba.mongodb.net/demoproject?retryWrites=true&w=majority")
     .then(() => {
         console.log(`Database Connected`);
     })
